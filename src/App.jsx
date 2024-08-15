63761d7944e73c1df310a930a85df0dbf27cfeb4
Sharif Akram <rifkit@gmail.com>
2024-08-15 09:59:44 -0400

    list objects using map function

--- src/App.jsx
+++ src/App.jsx
@@ -29,11 +29,21 @@ function App() {
 
       <label htmlFor='search'>Search: </label>
       <input id='search' type='text' />
+
       <hr />
 
       <ul>
         {list.map(function (item) {
-          return <li key={item.objectID}>{item.title}</li>;
+          return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title} </a>
+            </span>
+            <span>{item.author}</span>
+            {/*<span>{item.num_comments}</span>*/}
+            {/*<span>{item.points}</span>*/}
+            </li>
+          );
         })}
       </ul>
     </div>
